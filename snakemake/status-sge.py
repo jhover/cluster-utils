0f422bbc3e18282739385c6c77847da9115c1443
jhover <jhover@transit>
2022-08-15 14:00:48 -0400

    Update.

--- snakemake/status-sge.py
+++ snakemake/status-sge.py
@@ -1,39 +1,30 @@
 #!/usr/bin/env python
+#
+# status script for SGE/UGE
+# takes jobid, returns success, failed, running. 
+#
+
+
+
 import subprocess
 import sys
 
-
 def run_command_shell(cmd):
     """
-    maybe subprocess.run(" ".join(cmd), shell=True)
-    cmd should be standard list of tokens...  ['cmd','arg1','arg2'] with cmd on shell PATH.
     
     """
     cmdstr = " ".join(cmd)
-    #logging.info(f"running command: {cmdstr} ")
-    start = dt.datetime.now()
     cp = subprocess.run(" ".join(cmd), 
                     shell=True, 
                     stdout=subprocess.PIPE, 
                     stderr=subprocess.STDOUT)
-    #cp = subprocess.run(cmd, 
-    #                shell=True, 
-    #                stdout=subprocess.PIPE, 
-    #                stderr=subprocess.STDOUT)
-    end = dt.datetime.now()
-    elapsed =  end - start
-    #logging.debug(f"ran cmd='{cmdstr}' return={cp.returncode} {elapsed.seconds} seconds.")
     return cp
 
 jobid = sys.argv[1]
-
-#output = str(subprocess.check_output("sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
-
 status = "unknown"
 
 statcmd = ['qstat', '-j', f'{jobid}'  ]
 acctcmd = ['qacct', '-j', f'{jobid}'  ]
-
 
 cp = run_command_shell( statcmd )
 if str(cp.returncode) == '0':
